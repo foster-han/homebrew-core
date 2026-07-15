847feb996164a658de59aceac4d882ef83826e86
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-15 20:36:34 +0000

    mesa 26.1.5

--- Formula/m/mesa.rb
+++ Formula/m/mesa.rb
@@ -3,8 +3,8 @@ class Mesa < Formula
 
   desc "Graphics Library"
   homepage "https://www.mesa3d.org/"
-  url "https://archive.mesa3d.org/mesa-26.1.4.tar.xz"
-  sha256 "072705caa9adf4740f1489194b13e278ad959166863b5271fe423a86353c9ab6"
+  url "https://archive.mesa3d.org/mesa-26.1.5.tar.xz"
+  sha256 "79e421c7ce18cd9e790b8375920325779f10798630bf30e0b22f1a21c8617122"
   license all_of: [
     "MIT",
     "Apache-2.0", # include/{EGL,GLES*,vk_video,vulkan}, src/egl/generate/egl.xml, src/mapi/glapi/registry/gl.xml
